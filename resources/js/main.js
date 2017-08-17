0f308de939717c1c7719fd85c2346364b250fbe5
Zia <zia_gt@yahoo.com>
2017-08-17 18:20:37 +0200

    Scroll animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,6 +8,18 @@ window.addEventListener("hashchange", function() { scrollBy(0, -80) })
       $(this).addClass("active");
 	});
 
+$("nav ul li a[href^='#']").on('click', function(e) {
+   e.preventDefault();
+   var hash = this.hash;
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 500, function(){
+
+       window.location.hash = hash;
+     });
+
+});
+
 // Navbar end
 // Contact form start
 var $contactForm = $('#contactForm');
