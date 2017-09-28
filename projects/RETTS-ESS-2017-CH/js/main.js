247ce754b3a71cce0492c6c056037ecb1508725f
Zia <zia_gt@yahoo.com>
2017-09-28 21:57:47 +0200

    xml file

--- projects/RETTS-ESS-2017-CH/js/main.js
+++ projects/RETTS-ESS-2017-CH/js/main.js
@@ -6,7 +6,7 @@
 
 function parseXML() {
   $.ajax({
-      url: "../RETTS-ESS-2017-Chinese.xml",
+      url: "https://github.com/ziaongit/ziaongit.github.io/blob/master/projects/RETTS-ESS-2017-CH/RETTS-ESS-2017-Chinese.xml",
       dataType: "xml",
       success: function(data) {
           parseXml(data);
